e78b96ca85a9ce5c6fe1497d30b485ed67f844cb
MaksymProtsak <protsak.m@ajax.systems>
2026-02-27 14:19:20 +0200

    Updated main.py.

--- main.py
+++ main.py
@@ -1,22 +1,43 @@
 import os
+from contextlib import contextmanager
 from pprint import pprint
+from typing import Tuple, List
 
-import psycopg2
 from dotenv import load_dotenv
+from psycopg2 import sql
+from psycopg2.pool import SimpleConnectionPool
+
 
 load_dotenv()
 
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_USER = os.environ.get("DB_USER")
-DB_PASS = os.environ.get("DB_PASS")
+pool = SimpleConnectionPool(
+    minconn=1,
+    maxconn=5,
+    host=os.environ.get("DB_HOST"),
+    port=os.environ.get("DB_PORT"),
+    dbname=os.environ.get("DB_NAME"),
+    user=os.environ.get("DB_USER"),
+    password=os.environ.get("DB_PASS"),
+)
 
-def get_tables():
-    with psycopg2.connect(
-        host=DB_HOST, port=DB_PORT, dbname=DB_NAME, user=DB_USER, password=DB_PASS
-    ) as conn:
-        with conn.cursor() as cur:
+
+@contextmanager
+def get_cursor():
+    conn = pool.getconn()
+    try:
+        cur = conn.cursor()
+        yield cur
+        conn.commit()
+    except Exception:
+        conn.rollback()
+        raise
+    finally:
+        cur.close()
+        pool.putconn(conn)
+
+
+def fetch_tables():
+    with get_cursor() as cur:
             cur.execute(
                 """
                 SELECT table_name
@@ -25,8 +46,42 @@ def get_tables():
                 ORDER BY table_name;
                 """
             )
+            return [table[0] for table in cur.fetchall()]
+
+def fetch_table(table_name:str) -> List[Tuple[str]]:
+    if table_name not in [table for table in fetch_tables()]:
+        return []
+
+    with get_cursor() as cur:
+            cur.execute(
+                sql.SQL("SELECT * FROM {}").format(sql.Identifier(table_name))
+            )
             res = cur.fetchall()
-            pprint(res)
+            return res
+
+
+def fetch_user(user_id:int) -> List[Tuple[str]]:
+    with get_cursor() as cur:
+            cur.execute(
+                "SELECT * FROM users WHERE user_id=%s", (user_id,)
+            )
+            return cur.fetchall()[0]
+
+
+def is_table_contains_column(table_name:str, column:str) -> bool:
+    with get_cursor() as cur:
+        cur.execute(
+            "SELECT 1 "
+            "FROM information_schema.columns "
+            "WHERE table_name = %s AND column_name = %s;",
+            (table_name, column)
+        )
+        return bool(cur.fetchall())
+
+
 
 if __name__ == "__main__":
-    get_tables()
+    pprint(fetch_tables())
+    pprint(fetch_table("users"))
+    pprint(fetch_user(4))
+    pprint(is_table_contains_column("users", "username"))
